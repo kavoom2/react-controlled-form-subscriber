2dca3ba50c0e66ff5c47314353140a0babecf992
정진규 <jg.jung@muzlive.com>
2023-03-02 01:30:28 +0900

    feat: rollup Build 이슈 해결

--- rollup.config.js
+++ rollup.config.js
@@ -1,45 +1,44 @@
+import babel from "@rollup/plugin-babel";
+import commonjs from "@rollup/plugin-commonjs";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import typescript from "@rollup/plugin-typescript";
+import { readFileSync } from "fs";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
 
-import babel from '@rollup/plugin-babel';
-import commonjs from '@rollup/plugin-commonjs';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
-import typescript from '@rollup/plugin-typescript';
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-
-const extensions = [ 'js', 'jsx', 'ts', 'tsx', 'mjs' ];
-const pkg = require('./package.json')
+const pkg = JSON.parse(readFileSync('package.json', {encoding: 'utf8'}));
+const extensions = ["js", "jsx", "ts", "tsx", "mjs"];
 const config = [
-    {
-        external: [ /node_modules/ ],
-        input: './src/index.ts',
-        output: [
-            {
-                dir: './dist',
-                format: 'cjs',
-                preserveModules: true,
-                preserveModulesRoot: 'src'
-            },
-            {
-                file: pkg.module,
-                format: 'es'
-            }
-            ,
-            {
-                name: pkg.name,
-                file: pkg.browser,
-                format: 'umd'
-            }
-        ],
-        plugins: [
-            nodeResolve({ extensions }),
-            babel({
-                exclude: 'node_modules/**',
-                extensions,
-                include: [ 'src/**/*' ]
-            }),
-            commonjs({ include: 'node_modules/**' }),
-            peerDepsExternal(),
-            typescript({ tsconfig: './tsconfig.json' }),
-        ]
-    }
+  {
+    external: [/node_modules/],
+    input: "./src/index.ts",
+    output: [
+      {
+        dir: "./dist",
+        format: "cjs",
+        preserveModules: true,
+        preserveModulesRoot: "src",
+      },
+      {
+        file: pkg.module,
+        format: "es",
+      },
+      {
+        name: pkg.name,
+        file: pkg.browser,
+        format: "umd",
+      },
+    ],
+    plugins: [
+      nodeResolve({ extensions }),
+      babel({
+        exclude: "node_modules/**",
+        extensions,
+        include: ["src/**/*"],
+      }),
+      commonjs({ include: "node_modules/**" }),
+      peerDepsExternal(),
+      typescript({ tsconfig: "./tsconfig.json" }),
+    ],
+  },
 ];
 export default config;
